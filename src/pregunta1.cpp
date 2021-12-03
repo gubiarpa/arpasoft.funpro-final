1857a0ca5fb620dec0af3b0c2013ed8218c63027
Billy Arredondo <billy.arredondo@pucp.pe>
2021-12-03 12:52:54 -0500

    update pregunta 1
    correction on ordernarNotas sense and filtrarNotas condition

--- src/pregunta1.cpp
+++ src/pregunta1.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #define MAX_ELEMENTOS 12 // Cantidad de notas
-#define MULTIPLO 5 // múltiplo escogido
 #define NOTA_INVALIDA -1 // Valor de nota inválida
 using namespace std;
 
-void quickSort(int A[],int izq, int der) { 
+void ordenarNotas(int A[],int izq, int der) { 
 	int i, j, x , aux; 
 	i = izq;
 	j = der;
@@ -12,12 +11,12 @@ void quickSort(int A[],int izq, int der) {
 	
 	// Ordenamiento con pivote
 	do { 
-		while((A[i] < x) && (j <= der))
+		while((A[i] > x) && (j <= der))
 		{
 			i++;
 		}
 		
-		while((x < A[j]) && (j > izq))
+		while((x > A[j]) && (j > izq))
 		{
 			j--;
 		} 
@@ -31,60 +30,38 @@ void quickSort(int A[],int izq, int der) {
 	
 	// Recursión a la izquierda
 	if( izq < j ) 
-		quickSort(A, izq, j); 
+		ordenarNotas(A, izq, j); 
 	
 	// Recursión a la derecha
 	if( i < der ) 
-		quickSort(A, i, der); 
+		ordenarNotas(A, i, der); 
 }
 
-/*
-void volteaArregloProcesado(int A[]) {
-	// Busca máxima posición
-	int maxPosicion = 0;
-	while (A[maxPosicion] != NOTA_INVALIDA) {
-		maxPosicion++;
-	}
-	
-	// Invierte el arreglo
-	for (int i = 0; i <= maxPosicion / 2; i++) {
-		int temp = A[i];
-		A[i] = A[maxPosicion - i];
-		A[maxPosicion] = temp;
-	}
-}
-*/
+
 
 void filtrarNotas(int A[], int multiplo) {	
-	// Clonamos arreglo
+	// Clonamos arreglo y limpiamos el original
 	int B[MAX_ELEMENTOS];
 	for (int i = 0; i < MAX_ELEMENTOS; i++) {
 		B[i] = A[i];
-	}
-	
-	// Limpiamos el arreglo original (con -1)
-	for (int i = 0; i < MAX_ELEMENTOS; i++) {
 		A[i] = NOTA_INVALIDA;
 	}
 	
 	// Filtramos
 	int iA = 0;
 	for (int iB = 0; iB < MAX_ELEMENTOS; iB++) {
-		if ( (iB == 0) || ( (B[iB] != B[iB - 1]) && (B[iB] % multiplo == 0) ) ) {
-			A[iA] = B[iB];
-			iA++;
+		if (
+			( (iB == 0) || (B[iB] != B[iB - 1]) ) &&
+			( B[iB] % multiplo == 0 )
+		) {
+			A[iA++] = B[iB];
 		}
 	}
-	
-	/*
-	// Invertimos el orden del arreglo procesado
-	volteaArregloProcesado(A);
-	*/
 }
 
 void procesarNotas(int A[], int multiplo) {
 	// 1. Primero, ordenamos
-	quickSort(A, 0, MAX_ELEMENTOS - 1);
+	ordenarNotas(A, 0, MAX_ELEMENTOS - 1);
 	
 	// 2. Remover duplicados
 	filtrarNotas(A, multiplo);
@@ -93,15 +70,16 @@ void procesarNotas(int A[], int multiplo) {
 void imprimirNotasProcesadas(int A[]) {
 	int i = 0;
 	while ( (i < MAX_ELEMENTOS) && (A[i] != NOTA_INVALIDA) ) {
-		cout << A[i] << " ";
-		i++;
+		cout << A[i++] << " ";
 	}
 }
 
 int main()
 {	
-	int notas[MAX_ELEMENTOS] = {15, 12, 4, 10, 9, 14, 9, 12, 4, 10, 11, 8};	
-	procesarNotas(notas, MULTIPLO);
+	int multiplo;
+	int notas[MAX_ELEMENTOS] = {2, 15, 12, 10, 9, 14, 9, 12, 4, 10, 11, 8};	
+	cout << "Múltiplo: "; cin >> multiplo;
+	procesarNotas(notas, multiplo);
 	imprimirNotasProcesadas(notas);
 
 	return 0;
